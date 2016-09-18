06dc49b42525f28eaeab31c576b7ca085a837977
eugbor <eugeneborodin@mail.ru>
2016-09-19 01:16:48 +0300

    init

---
+++ AlwaysSortArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlwaysSortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            int n;
+            while (a)
+            {
+                n = int.Parse(Console.ReadLine());
+                if(n <= -1) { a = false;}
+                var b = GetList(n).ToArray();
+                for (int i = 0; i < b.Length; i++)
+                    Console.WriteLine("{0}", b[i]);
+            }
+            Console.ReadLine();
+        }
+
+        public static List<string> GetList(int n)
+        {
+            List<string> list = new List<string>();
+            if (n == 0)
+            {
+                list.Insert(0, "0"); 
+                return list;
+            }
+
+            list.Insert(list.Count, n.ToString());
+            list.Sort();
+            return GetList(n - 1);
+        }
+
+    }
+}
